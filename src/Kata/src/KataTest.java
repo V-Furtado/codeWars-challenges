44e9a34c82a07f4cbc5c439082b524516c69216c
Vanderley Furtado <50887339+V-Furtado@users.noreply.github.com>
2021-09-12 00:45:52 -0400

    feat: delete unused imports

--- src/Kata/src/KataTest.java
+++ src/Kata/src/KataTest.java
@@ -1,10 +1,5 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-
-//import org.junit.Test;
-//
-//import static org.junit.Assert.assertEquals;
 
 public class KataTest {
     @Test
