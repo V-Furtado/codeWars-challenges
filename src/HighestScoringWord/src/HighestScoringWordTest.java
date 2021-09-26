5d9ae461117bba656d0ec20ad98dcf29be0f0296
vanderley Furtado <vanderleyfurtado@vanderleys-MacBook-Pro.local>
2021-09-25 21:53:54 -0400

    feat: Remove unused imports in HighestScoreWord file

--- src/HighestScoringWord/src/HighestScoringWordTest.java
+++ src/HighestScoringWord/src/HighestScoringWordTest.java
@@ -1,10 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
 
-// import org.junit.Test;
-// import static org.junit.Assert.assertEquals;
-// import org.junit.runners.JUnit4;
 
 public class HighestScoringWordTest {
     @Test
